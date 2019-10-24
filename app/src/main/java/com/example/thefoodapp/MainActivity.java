ac088c85c53a5087befe8b55f6dfadfcbb6a305f
Emma <erj2016@ksu.edu>
2019-10-23 20:13:46 -0500

    Added started file for project.

--- app/src/main/java/com/example/thefoodapp/MainActivity.java
+++ app/src/main/java/com/example/thefoodapp/MainActivity.java
@@ -3,11 +3,39 @@ package com.example.thefoodapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class MainActivity extends AppCompatActivity {
 
+    private Document doc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean crawl(String url)
+    {
+        try
+        {
+            Connection connection = Jsoup.connect(url);
+            Document doc = connection.get();
+            String searchWord = "test";
+            this.doc = doc;
+
+            System.out.println("Searching for the word " + searchWord + "...");
+            String bodyText = this.doc.body().text();
+            return bodyText.toLowerCase().contains(searchWord.toLowerCase());
+        }
+        catch(IOException ex)
+        {
+            return false;
+        }
+
+
     }
 }
